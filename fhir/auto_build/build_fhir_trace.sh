34ed6c98b165f80edb313abd60c38de8c2bb0ad2
25077667 <zxc25077667@protonmail.com>
2021-09-11 03:34:49 +0800

    Add FHIR submodules and auto_build

---
+++ fhir/auto_build/build_fhir_trace.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#TODO: Get the version code of IBM/FHIR
+
+WORK_PWD='/media/d/git/nsysu/cs/fhir/FHIR/fhir-install'
+
+orig_fhir_server_file="${WORK_PWD}/Dockerfile"
+new_fhir_server_file="${WORK_PWD}/Dockerfile.tracee"
+cp $orig_fhir_server_file $new_fhir_server_file
+
+# Modify to trace version
+sed -i "s,openliberty/open-liberty:.*ubi,openliberty-alpine,g" $new_fhir_server_file
+sed -i '/RUN yum install -y unzip/d' $new_fhir_server_file
+sed -i '/USER 1001/d' $new_fhir_server_file
+sed -i '64iUSER root' $new_fhir_server_file
+
+# Backup bootstraps
+orig_bootstraps="${WORK_PWD}/src/main/docker/ibm-fhir-server/bootstrap.sh"
+cp $orig_bootstraps "$orig_bootstraps".bak
+
+# Enable tracee in bootstraps
+sed -i '15i/tracee/tracee-ebpf -t c --output json 1>/mnt/out 2>/mnt/err &' $orig_bootstraps
+sed -i '16isleep 5' $orig_bootstraps
+
+docker build -t fhir-server-base:03 $WORK_PWD -f "$WORK_PWD"/Dockerfile.tracee
+
+cp "$orig_bootstraps".bak $orig_bootstraps
---
+++ fhir/auto_build/build_jdk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#TODO: Get the version code of jdk
+WORK_PWD='/media/d/git/nsysu/cs/fhir/ci.docker/releases/21.0.0.6/kernel-slim/'
+
+ubi_docker_file="${WORK_PWD}Dockerfile.ubi.adoptopenjdk11"
+alpine_docker_file="${WORK_PWD}Dockerfile.alpine.adoptopenjdk11"
+# alpine_docker_file="${WORK_PWD}Dockerfile.alpinee.adoptopenjdk11"
+
+# Change the jdk image
+cp $ubi_docker_file $alpine_docker_file
+sed -i 's/FROM\(.*\)ubi/FROM\1alpine/g' $alpine_docker_file
+sed -i 's/yum -y install shadow-utils wget unzip openssl/apk update \&\& apk add --no-cache shadow wget unzip openssl bash elfutils-dev/' $alpine_docker_file
+sed -i '/&& yum -y remove.*/d' $alpine_docker_file
+sed -i '/&& yum clean.*/d' $alpine_docker_file
+sed -i 's/| sha1sum -c --strict --check //' $alpine_docker_file
+sed -i 's/adduser -u 1001 -r -g root/adduser -u 1001 -S -G root/g' $alpine_docker_file
+
+# Add tracee into jdk
+sed -i '25iCOPY --from=aquasec/tracee:latest /tracee/ /tracee' $alpine_docker_file
+
+# Build alpine jdk
+docker build -t openliberty-alpine ${WORK_PWD} -f $alpine_docker_file
