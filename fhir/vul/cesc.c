4d5bb39f31e501f35bae14f881afa965fa3f56a3
25077667 <zxc25077667@gmail.com>
2021-09-19 05:02:44 +0800

    Format vuls

--- fhir/vul/cesc.c
+++ fhir/vul/cesc.c
@@ -8,30 +8,29 @@
 #define MOUNT_TARGET "/dev/sda2"
 #define SECRET_PATH "/home/scc/secret"
 
-#define TRY_THROW(op, is_fail)   \
-	do                           \
-	{                            \
-		__auto_type _err = (op); \
-		if (_err == (is_fail))   \
-		{                        \
-			perror(#op);         \
-			exit((int64_t)_err); \
-		}                        \
-	} while (0)
+#define TRY_THROW(op, is_fail)       \
+    do {                             \
+        typeof(is_fail) _err = (op); \
+        if (_err == (is_fail)) {     \
+            perror(#op);             \
+            exit(_err);              \
+        }                            \
+    } while (0)
 
 char buf[100];
 
 int main()
 {
-	TRY_THROW(mkdir(MOUNT_POINT, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH), -1);
-	TRY_THROW(mount(MOUNT_TARGET, MOUNT_POINT, "ext4", MS_SYNCHRONOUS, ""), -1);
+    TRY_THROW(mkdir(MOUNT_POINT, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH), -1);
+    TRY_THROW(mount(MOUNT_TARGET, MOUNT_POINT, "ext4", MS_SYNCHRONOUS, ""), -1);
 
-	FILE *secret_file;
-	TRY_THROW((secret_file = fopen(MOUNT_POINT SECRET_PATH, "r")), 0);
+    FILE *secret_file;
+    TRY_THROW((secret_file = fopen(MOUNT_POINT SECRET_PATH, "r")), NULL);
 
-	char *ret;
-	TRY_THROW(((ret = fgets(buf, sizeof(buf), secret_file)) && puts(buf)), (ret != buf));
-	fclose(secret_file);
+    char *ret;
+    TRY_THROW(((ret = fgets(buf, sizeof(buf), secret_file)) && puts(buf)),
+              (ret != buf));
+    fclose(secret_file);
 
-	return 0;
+    return 0;
 }
