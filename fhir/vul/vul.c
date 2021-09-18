e24468bec2a42d76bee5120661de5365587e65ad
25077667 <zxc25077667@gmail.com>
2021-09-19 05:02:44 +0800

    Format vuls

--- fhir/vul/vul.c
+++ fhir/vul/vul.c
@@ -1,9 +1,10 @@
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
 char buf[100] = {0};
 
-int main(){
-	scanf("%s", buf);
-	return execve(buf, 0, 0);
+int main()
+{
+    scanf("%s", buf);
+    return execve(buf, 0, 0);
 }
